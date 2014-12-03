b1816911c1fa6db78ca64bd386e00c292c02ae73
sparkhoom <176072396@qq.com>
2014-12-03 12:12:47 +0800

    add user create json api, and user name will automatically generate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 #encoding: utf-8
 class UsersController < ApplicationController
+  respond_to :json
+  skip_before_action :verify_authenticity_token
+
   def welcome
   end
 
@@ -11,8 +14,7 @@ class UsersController < ApplicationController
   end
 
   def create_login_session
-    p params
-    user = User.find_by_name(login_params[:name])
+    user = User.find_by_name(login_params [:name])
     if user && user.authenticate(params[:password])
       cookies.permanent[:token] = user.token
       redirect_to root_url, :notice => "登录成功"
@@ -29,16 +31,23 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      cookies.permanent[:token] = @user.token
-      redirect_to :root, :notice => "注册成功"
-    else
-      render :signup
+    respond_to do |format|
+      if @user.save
+        format.html {
+          cookies.permanent[:token] = @user.token
+          redirect_to :root, :notice => "注册成功"
+        }
+        format.json { render status: 200}
+      else
+        format.html {render :signup }
+        format.json {render json: @user.errors }
+      end
     end
   end
 
   private
   def user_params
+    p params
     params.require(:user).permit(:name, :password, :password_confirmation, :phone_number, :email)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  before_create { generate_token(:token) }
+  before_create { generate_token(:token)}
+  before_create { generate_name(:name)}
 
-  validates :name, :uniqueness => {:case_sensitive => false}
   validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :email_format => true
   validates :password, :length => { :minimum => 6 }, :on => :create
   validates :phone_number, :presence => true, :phone_number_format => true
@@ -11,6 +11,10 @@ class User < ActiveRecord::Base
     begin
       self[column] = SecureRandom.urlsafe_base64
     end while User.exists?(column => self[column])
+  end
+
+  def generate_name(column)
+      self[column] = "user" + self[:token]
   end
   
 end
