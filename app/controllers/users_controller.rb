599693912e3b232f7c647eedfba1b7f303956a37
sparkhoom <176072396@qq.com>
2014-12-05 14:48:30 +0000

    add json api: user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 #encoding: utf-8
 class UsersController < ApplicationController
   respond_to :json
-  skip_before_action :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token,
+                     :if => Proc.new { |c| c.request.format == 'application/json' }
 
   def welcome
   end
@@ -14,13 +15,21 @@ class UsersController < ApplicationController
   end
 
   def create_login_session
-    user = User.find_by_name(login_params [:name])
-    if user && user.authenticate(params[:password])
-      cookies.permanent[:token] = user.token
-      redirect_to root_url, :notice => "登录成功"
-    else
-      flash[:error] = "无效的用户名和密码"
-      redirect_to :login
+    user = User.find_by_name(login_params[:name])
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        format.html{
+          cookies.permanent[:token] = user.token
+          redirect_to root_url, :notice => "登录成功"
+        }
+        format.json{ render json:user}
+      else
+        format.html{
+          flash[:error] = "无效的用户名和密码"
+          redirect_to :login
+        }
+        format.json { render json:user.errors}
+      end
     end
   end
 
@@ -47,7 +56,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    p params
     params.require(:user).permit(:name, :password, :password_confirmation, :phone_number, :email)
   end
 
